03c0a8bd0e5047a3489f4fd14b4e5a60baf3412b
Shreeda <nshreedakumar@gmail.com>
2025-10-27 21:54:06 +0530

    Level 1 completed with hashing password

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require("express");
+
 const app = express();
+
 const port = 8080;
 
+const bcrypt = require('bcrypt');
+
+const saltRounds = 10; 
+
 app.use(express.json());
+
 app.use(express.urlencoded({extended:true}));
 
 app.set("view engine","ejs");
@@ -13,54 +20,49 @@ app.listen(port,() => {
 
 const userInfo = [];
 
-    app.get("/",(req,res) => {
-        res.redirect("/signup");
-        console.log(" call reached signup page")
-    })
+app.get("/",(req,res) => {
+     res.redirect("/signup");
+})
 
-    app.get("/signup",(req,res) => {
-        res.render("signup.ejs");
-        console.log("Opened signup page.");
-    })
+app.get("/signup",(req,res) => {
+    res.render("signup.ejs");
+})
 
 app.post("/signup",(req,res) => {
    const  {username,password} = req.body; 
-   userInfo.push({username,password});
-   res.render("home.ejs",{username,password});
-   console.log(userInfo);
-   console.log("Post Reached home");
+   bcrypt.hash(password, saltRounds, (err, hash) => {
+  if (err) throw err;
+  userInfo.push({username,hash});
+  return hash;
+});
+   res.render("home.ejs");
 })
-console.log(userInfo);
 
 app.get("/login",(req,res) => {
     res.render("login.ejs");
-    console.log("opened login page");
 })
 
 app.post("/home",(req,res) => {
-    let count =0;
-    console.log(req.body);
     const {username,password} = req.body;
-    for(let i=0;i<userInfo.length;i++){
-        if((req.body.username == userInfo[i].username)&&(req.body.password == userInfo[i].password)){
-            count++;
-            console.log(count);
-            console.log(userInfo[i].username);
-            break;
-            }
-    }
-    
-    console.log(count);
-    if(count == 0){
+    const user = userInfo.find(u => u.username === username);
+    if(!user){
         res.send("Invalid User Info");
-    }else{
-        res.render("home.ejs",{username,password});
     }
-
+    bcrypt.compare(password, user.hash, (err, result) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).send("Server Error during login.");
+        }
+        if(result === true){
+            res.render("home.ejs", { username: user.username });
+        }else {
+            res.send("Invalid User Info");
+        }
+                }
+    )
 })
 
  app.get("/home",(req,res) => {
     res.render("home.ejs");
-    console.log("Rendering home page.");
     })
 
